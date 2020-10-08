2950b6b0b8c5da21f0e901dd88b5e2983c0895cc
Ravi Kumar27 <Ravi_Kumar27@ad.infosys.com>
2020-10-08 02:06:19 +0200

    angular spring and postgre integration for employee sample

--- aws-eks-app-angular-springboot-posgres/frontend-angular/eks-demo/src/app/constant/api.constant.ts
+++ aws-eks-app-angular-springboot-posgres/frontend-angular/eks-demo/src/app/constant/api.constant.ts
@@ -1,4 +1,4 @@
 import {environment} from '../../environments/environment'
 export class Api {
-    public static employees: string =environment.API_BASE_URL+'/api/v1/employees';
+    public static employees: string =environment.API_BASE_URL+'/api/v2/employees';
 }
